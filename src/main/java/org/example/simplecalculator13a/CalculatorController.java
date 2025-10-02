70c4e8979566d3b5b457c69beabe7ccbda1dbc64
Zsolt Harmath <harmathzs@gmail.com>
2025-10-02 12:18:17 +0200

    Show view

--- src/main/java/org/example/simplecalculator13a/CalculatorController.java
+++ src/main/java/org/example/simplecalculator13a/CalculatorController.java
@@ -1,14 +1,18 @@
 package org.example.simplecalculator13a;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CalculatorController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void handleCalculate(ActionEvent actionEvent) {
     }
 }
