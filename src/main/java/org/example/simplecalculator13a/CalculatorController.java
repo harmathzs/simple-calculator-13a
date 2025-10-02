7447c3bc1adc9f0c07e8fe63ce324b057cd4bda0
Zsolt Harmath <harmathzs@gmail.com>
2025-10-02 12:19:43 +0200

    Bind fields

--- src/main/java/org/example/simplecalculator13a/CalculatorController.java
+++ src/main/java/org/example/simplecalculator13a/CalculatorController.java
@@ -2,9 +2,15 @@ package org.example.simplecalculator13a;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CalculatorController {
+    @FXML public TextField firstTextField;
+    @FXML public ChoiceBox<String> operationChoiceBox;
+    @FXML public TextField secondTextField;
+    @FXML public Label resultLabel;
     @FXML
     private Label welcomeText;
 
