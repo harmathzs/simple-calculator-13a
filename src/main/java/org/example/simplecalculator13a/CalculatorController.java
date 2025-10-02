b64d808f2de612b8a71ff80a460c9479eb72c3ed
Zsolt Harmath <harmathzs@gmail.com>
2025-10-02 12:24:10 +0200

    Init operationChoiceBox

--- src/main/java/org/example/simplecalculator13a/CalculatorController.java
+++ src/main/java/org/example/simplecalculator13a/CalculatorController.java
@@ -1,12 +1,20 @@
 package org.example.simplecalculator13a;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class CalculatorController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class CalculatorController implements Initializable {
     @FXML public TextField firstTextField;
     @FXML public ChoiceBox<String> operationChoiceBox;
     @FXML public TextField secondTextField;
@@ -20,5 +28,18 @@ public class CalculatorController {
     }
 
     public void handleCalculate(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // init operationChoiceBox
+        List<String> operations = new ArrayList<>();
+        operations.add("+");
+        operations.add("-");
+        operations.add("*");
+        operations.add(":");
+        ObservableList<String> oListOfOperations = FXCollections.observableList(operations);
+        operationChoiceBox.setItems(oListOfOperations);
+        operationChoiceBox.setValue("+");
     }
 }
