8e5c01452bb919777f544812355072cbad8d7f68
Zsolt Harmath <harmathzs@gmail.com>
2025-10-02 13:04:18 +0200

    Test CalculatorApplication

--- src/main/java/org/example/simplecalculator13a/CalculatorApplication.java
+++ src/main/java/org/example/simplecalculator13a/CalculatorApplication.java
@@ -8,16 +8,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class CalculatorApplication extends Application {
+    public static boolean isRunningTest = false;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 150);
-        stage.setTitle("Calculator");
-        stage.setScene(scene);
-        stage.show();
+        FXMLLoader fxmlLoader = null;
+        if (!isRunningTest) fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator-view.fxml"));
+        Scene scene = null;
+        if (!isRunningTest) scene = new Scene(fxmlLoader.load(), 500, 150);
+        if (!isRunningTest) stage.setTitle("Calculator");
+        if (!isRunningTest) stage.setScene(scene);
+        if (!isRunningTest) stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        if (!isRunningTest) launch();
     }
 }
--- src/main/java/org/example/simplecalculator13a/CalculatorTest.java
+++ src/main/java/org/example/simplecalculator13a/CalculatorTest.java
@@ -2,9 +2,21 @@ package org.example.simplecalculator13a;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class CalculatorTest {
     @Test
-    public void testApp() {
-
+    public void testAppMain() {
+        CalculatorApplication app = new CalculatorApplication();
+        CalculatorApplication.isRunningTest = true;
+        CalculatorApplication.main(null);
+        CalculatorApplication.isRunningTest = false;
+    }
+    @Test
+    public void testAppStart() throws IOException {
+        CalculatorApplication app = new CalculatorApplication();
+        CalculatorApplication.isRunningTest = true;
+        app.start(null);
+        CalculatorApplication.isRunningTest = false;
     }
 }
